a50d00517cb627cee2a17bca4d468da01dcd2392
ASAKI Yoshiharu <asaki@piano.local>
2022-08-27 22:25:10 -0400

    Update 2022-08-27

--- source_develop/src/pgtools/pg_panel_tile.c
+++ source_develop/src/pgtools/pg_panel_tile.c
@@ -18,60 +18,73 @@ void  pg_panel_tile(int NPLANE, int IPLANE, float *fs,
     d1 = 0.845;
     d2 = 0.840;
     *bxmin = 0.119;
+    d1 = 0.795;
+    d2 = 0.790;
+    *bxmin = 0.120;
+    *bymax = 0.925;
     *fs = 0.7;
   } else if (NPLANE == 2 || NPLANE == 4) {
     *NX = 2;
     d1 = 0.395;
     d2 = 0.390;
     *bxmin = 0.119;
+    *bymax = 0.945;
     *fs = 0.7;
   } else if (NPLANE == 3 || NPLANE > 4 && NPLANE <= 9) {
     *NX = 3;
     d1 = 0.295;
     d2 = 0.290;
     *bxmin = 0.119;
+    *bymax = 0.945;
     *fs = 0.7;
   } else if (NPLANE > 9 && NPLANE <= 16) {
     *NX = 4;
     d1 = 0.205;
     d2 = 0.200;
     *bxmin = 0.119;
+    *bymax = 0.945;
     *fs = 0.6;
   } else if (NPLANE > 16 && NPLANE <= 25) {
     *NX = 5;
     d1 = 0.185;
     d2 = 0.180;
     *bxmin = 0.119;
+    *bymax = 0.945;
     *fs = 0.6;
   } else if (NPLANE > 25 && NPLANE <= 36) {
     *NX = 6;
     d1 = 0.145;
     d2 = 0.140;
     *bxmin = 0.119;
+    *bymax = 0.945;
     *fs = 0.5;
   } else if (NPLANE > 36 && NPLANE <= 49) {
     *NX = 7;
     d1 = 0.120;
     d2 = 0.115;
     *bxmin = 0.119;
+    *bymax = 0.945;
     *fs = 0.4;
   } else if (NPLANE > 49 && NPLANE <= 64) {
     *NX = 8;
     d1 = 0.105;
     d2 = 0.100;
     *bxmin = 0.119;
+    *bymax = 0.945;
     *fs = 0.4;
   } else if (NPLANE > 64 && NPLANE <= 90) {
     *NX = 9;
     d1 = 0.090;
     d2 = 0.088;
     *bxmin = 0.102;
+    *bymax = 0.945;
     *fs = 0.3;
   } else if (NPLANE > 91) {
     *NX = 10;
     d1 = 0.085;
     d2 = 0.083;
     *bxmin = 0.102;
+    *bymax = 0.945;
     *fs = 0.3;
   }
 
@@ -84,7 +97,6 @@ void  pg_panel_tile(int NPLANE, int IPLANE, float *fs,
   *nx = IPLANE % *NX;
   *ny = IPLANE / *NX;
 
-  *bymax = 0.945;
   *bymin = *bymax - d1 * (float)(*NY);
 
   *bxmax = *bxmin + d1 * (float)(*NX-1) + d2;
